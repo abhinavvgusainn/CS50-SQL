685091a8a999e82c6dfaf3f952d330aeb53d2ebd
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-06-12 13:05:47 +0530

    Add Players solutions

---
+++ 0-Querying/players/10.sql
@@ -0,0 +1,7 @@
+-- Find the tallest players born in the USA.
+
+SELECT "first_name","last_name", "height" AS "Height (inches)"
+FROM "players"
+WHERE "birth_country" = 'USA'
+ORDER BY "height" DESC, "first_name", "last_name"
+LIMIT 1;
