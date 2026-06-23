999fad27f1a0cf5bde88ea9595b9e0d98baf3d52
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-06-23 11:43:05 +0530

    Add Packages solution

--- 1-Relating/packages/log.sql
+++ 1-Relating/packages/log.sql
@@ -14,4 +14,13 @@ SELECT "contents" FROM "packages"
 WHERE "from_address_id" IS NULL;
 
 -- *** The Forgotten Gift ***
+SELECT "contents" FROM "packages"
+WHERE "from_address_id"= (SELECT "id" FROM "addresses" WHERE "address" = '109 Tileston Street')
+AND "to_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '728 Maple Place');
+
+SELECT "name","action" FROM "drivers" JOIN "scans" ON "drivers"."id" = "scans"."driver_id"
+WHERE "package_id" IN (
+    SELECT "id" FROM "packages"
+    WHERE "from_address_id"= (SELECT "id" FROM "addresses" WHERE "address" = '109 Tileston Street')
+    AND "to_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '728 Maple Place'))
 
