c936722a88be75b8e9da279be7414542c95f3fcf
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-06-23 10:40:00 +0530

    Add Packages solution

--- 1-Relating/packages/log.sql
+++ 1-Relating/packages/log.sql
@@ -2,9 +2,16 @@
 -- *** The Lost Letter ***
 SELECT "address", "type" FROM "addresses"
 WHERE id = (SELECT "to_address_id" FROM "packages" 
-WHERE "contents" = "Congratulatory letter")
+WHERE "contents" = "Congratulatory letter");
 
 -- *** The Devious Delivery ***
+SELECT "type" FROM "addresses"
+WHERE "id" =(SELECT "address_id" from "scans"
+WHERE "package_id" = (SELECT "id" FROM "packages"
+WHERE "from_address_id" IS NULL) AND "action" = 'Drop');
+
+SELECT "contents" FROM "packages"
+WHERE "from_address_id" IS NULL;
 
 -- *** The Forgotten Gift ***
 
