54641c94cd2ca3deded5b24574bde602ab2dc99c
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-06-28 11:07:55 +0530

    Add Packages solution

--- 1-Relating/packages/log.sql
+++ 1-Relating/packages/log.sql
@@ -22,5 +22,6 @@ SELECT "name","action" FROM "drivers" JOIN "scans" ON "drivers"."id" = "scans"."
 WHERE "package_id" IN (
     SELECT "id" FROM "packages"
     WHERE "from_address_id"= (SELECT "id" FROM "addresses" WHERE "address" = '109 Tileston Street')
-    AND "to_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '728 Maple Place'))
+    AND "to_address_id" = (SELECT "id" FROM "addresses" WHERE "address" = '728 Maple Place')
+);
 
