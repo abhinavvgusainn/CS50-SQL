dbcd3205d6a0ba51c302dbeda4f730dd63e58b08
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-06-28 11:06:35 +0530

    Add Moneyball solution

---
+++ 1-Relating/moneyball/4.sql
@@ -0,0 +1,7 @@
+SELECT players.first_name, players.last_name, salaries.salary
+FROM players
+JOIN salaries ON players.id = salaries.player_id 
+where year = 2001
+ORDER BY salary, first_name, last_name, player_id
+LIMIT 50;
+
